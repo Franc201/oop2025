bc5a18848d16c250ddd7d992ef69affba6945978
Franc201 <maxlurie2004@icloud.com>
2025-02-25 20:01:07 +0100

    Lab1(start)

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
         p1.x = 5;
         p1.y = 6;
-        System.out.println("Twoj x to " + p1.x + " a twoj y to " + p1.y);
-
+        System.out.println(p1);
+        p1.translate(400,500);
+        Point p2 = p1.translated(-400, -500);
+        System.out.println(p2.toSvg());
     }
+
 }
--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,27 @@
 public class Point {
     public double x;
     public double y;
+
+
+    public String toString() {
+        return "x = " + x + " y = " + y;
+    }
+    public String toSvg(){
+        return " <svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"45\" cx=\"" + x +"\" cy=\"" + y + "\" fill=\"blue\" />\n" +
+                "</svg> ";
+    }
+
+    public void translate(double dx,double dy) {
+        x += dx;
+        y += dy;
+
+    }
+
+    public Point translated(double dx,double dy) {
+        Point trPoint = new Point();
+        trPoint.x = x = dx;
+        trPoint.y = y + dy;
+        return trPoint;
+    }
 }
